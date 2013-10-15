633e7dcfdf9e3dd27c21a07e6a1b4c9bd19bf944
Xabier de Zuazo <xabier@onddo.com>
2013-10-15 02:30:14 +0200

    Checking attribute type inside attrs LWRP

--- providers/attrs.rb
+++ providers/attrs.rb
@@ -1,5 +1,8 @@
 
 action :read do
+  if not new_resource.attribute.kind_of?(Chef::Node::VividMash)
+    raise ArgumentError, "wrong node attribute: you should use writable node attributes like node.defaut[...] or node.normal[...]."
+  end
   zki = Chef::Zki.new(new_resource.server)
   new_resource.updated_by_last_action(
     zki.attributes_read(new_resource.path, new_resource.attribute, new_resource.key) === true
@@ -8,6 +11,9 @@ action :read do
 end
 
 action :write do
+  if not new_resource.attribute.kind_of?(Chef::Node::ImmutableMash)
+    raise ArgumentError, "wrong node attribute: you should use readable node attributes like node[...]."
+  end
   zki = Chef::Zki.new(new_resource.server)
   new_resource.updated_by_last_action(
     zki.attributes_write(new_resource.path, new_resource.attribute, new_resource.key) === true
