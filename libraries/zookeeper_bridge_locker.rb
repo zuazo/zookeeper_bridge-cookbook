402b632cd05209d9e0551f99ce1e51fdcdb22631
Xabier de Zuazo <xabier@onddo.com>
2014-08-17 19:02:23 +0200

    ZookeeperBridge::Locker refactored

--- libraries/zookeeper_bridge_locker.rb
+++ libraries/zookeeper_bridge_locker.rb
@@ -4,37 +4,32 @@ class Chef
   class ZookeeperBridge
     # ZooKeeper different locks logic
     class Locker < ZookeeperBridge
-      def shared_lock(path, wait)
+      private
+
+      def lock_wait_with_class(klass, path, wait, args = [], block)
         root_node, name = path_to_name_and_root_node(path)
-        lock = ZK::Locker::SharedLocker.new(@zk, name, root_node)
+        args = [@zk, name, args, root_node].flatten.compact
+        lock = klass.send(:new, *args)
         lock.with_lock(wait: wait) do
           Chef::Log.debug(
-            "Zookeeper Bridge #{__method__.to_s.gsub('_', ' ')} in \"#{path}\""
+            "Zookeeper Bridge #{caller[0].to_s.gsub('_', ' ')} in \"#{path}\""
           )
-          yield
+          block.call
         end
       end
 
-      def exclusive_lock(path, wait)
-        root_node, name = path_to_name_and_root_node(path)
-        lock = ZK::Locker::ExclusiveLocker.new(@zk, name, root_node)
-        lock.with_lock(wait: wait) do
-          Chef::Log.debug(
-            "Zookeeper Bridge #{__method__.to_s.gsub('_', ' ')} in \"#{path}\""
-          )
-          yield
-        end
+      public
+
+      def shared_lock(path, wait, &block)
+        lock_wait_with_class(ZK::Locker::SharedLocker, path, wait, nil, block)
       end
 
-      def semaphore(path, size, wait)
-        root_node, name = path_to_name_and_root_node(path)
-        lock = ZK::Locker::Semaphore.new(@zk, name, size, root_node)
-        lock.with_lock(wait: wait) do
-          Chef::Log.debug(
-            "Zookeeper Bridge #{__method__.to_s.gsub('_', ' ')} in \"#{path}\""
-          )
-          yield
-        end
+      def exclusive_lock(path, wait, &block)
+        lock_wait_with_class(ZK::Locker::ExclusiveLocker, path, wait, nil, block)
+      end
+
+      def semaphore(path, size, wait, &block)
+        lock_wait_with_class(ZK::Locker::Semaphore, path, wait, size, block)
       end
     end # Locker
   end # ZookeeperBridge
